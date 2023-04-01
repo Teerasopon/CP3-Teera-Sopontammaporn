1201fc3365e88b5a696324e98210d938bd526e5c
Teerasopon <129590684+Teerasopon@users.noreply.github.com>
2023-04-01 18:41:49 +0700

    Add files via upload

---
+++ Excercise4_Teera_S.py
@@ -0,0 +1,9 @@
+Foundation_English = 1.0
+General_Business = 2.0
+Introduction_to_Computer_Systems = 3.0
+Computer_Programing = 4.0
+print("--- Your Score ---")
+print("Foundation English : ",Foundation_English)
+print("General Business : ",General_Business)
+print("Introduction to Computer_Systems : ",Introduction_to_Computer_Systems)
+print("Computer Programing : ",Computer_Programing)
