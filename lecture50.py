adb1b8ab3bbb83b959473724cdd3bf1d89126be0
Teera Sopontammaporn <129590684+Teerasopon@users.noreply.github.com>
2023-07-19 20:31:51 +0700

    Add files via upload

---
+++ lecture50.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def mutiNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+addNumber(4,2)
+minusNumber(4,2)
+mutiNumber(4,2)
+divideNumber(4,2)
