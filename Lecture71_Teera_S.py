1f4e10cf345b8bee749c5d06e519bd1e7c2e95de
Teera Sopontammaporn <129590684+Teerasopon@users.noreply.github.com>
2023-07-24 19:59:56 +0700

    Add files via upload

---
+++ Lecture71_Teera_S.py
@@ -0,0 +1,22 @@
+menuList = []
+priceList = []
+
+def showBill():
+    totalPrice = 0
+    print("===== My Food =====")
+    for n in range(len(menuList)):
+        print(menuList[n],priceList[n])
+        totalPrice = totalPrice + int(priceList[n])
+    print("Totsal Price : ",totalPrice)
+
+
+while True:
+    menuName = input("Pleas Enter Menu : ")
+    if(menuName.lower() == "exit"):
+        break
+    else:
+        menuPrice = input("Price : ")
+        menuList.append(menuName)
+        priceList.append(menuPrice)
+print(menuList,priceList)
+showBill()
